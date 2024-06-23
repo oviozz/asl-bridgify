e6ae59bedb6a4523099c5bfadad35737f9f61a27
hemchande <hemchandeisha@gmail.com>
2024-06-23 06:31:40 -0700

    eisha

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ from camera import VideoCamera
 from flask_cors import CORS
 import requests
 import re
+import cv2
+import mediapipe as mp
+from wordDetectionLoading.model import callModel
 
 app = Flask(__name__)
 CORS(app)
+
+UPLOAD_FOLDER = 'uploads'
+FRAMES_FOLDER = 'frames'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['FRAMES_FOLDER'] = FRAMES_FOLDER
 
 def gen(camera):
     while True:
@@ -21,7 +29,7 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=5001, threaded=True, use_reloader=False)
 
 def scrape_signing_savvy(word):
     base_url = 'https://www.signingsavvy.com'
@@ -48,6 +56,72 @@ def scrape_signing_savvy(word):
         return "Failed to retrieve the webpage"
 
 
+
+def extract_frames(video_path):
+    # Initialize MediaPipe Hands
+    mp_hands = mp.solutions.hands
+    #hands = mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.8)
+    mp_drawing = mp.solutions.drawing_utils
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        print(f"Error: Could not open video {video_path}")
+        return []
+
+    frame_count = 0
+    all_hand_landmarks = []
+
+    with mp_hands.Hands(static_image_mode=True, max_num_hands=2, min_detection_confidence=0.8) as hands:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = hands.process(image)
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            frame_hand_landmarks = []
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                    landmarks = []
+                    for landmark in hand_landmarks.landmark:
+                        # landmarks.append({
+                        #     'x': landmark.x,
+                        #     'y': landmark.y,
+                        #     'z': landmark.z
+                        # })
+                        if landmark.x == None or landmark.y == None:
+                            landmarks.extend([0.0,0.0])
+                        else:
+
+                            landmarks.extend([landmark.x,landmark.y])
+
+                    if len(landmarks) < 84:
+                        remaining = 84 - len(landmarks)
+                        landmarks.extend([0.0] * remaining)
+                        
+
+                    frame_hand_landmarks.append(landmarks)
+
+                    
+                    
+                    #if frame_hand_landm
+
+
+            all_hand_landmarks.append(frame_hand_landmarks)
+            frame_count += 1
+        if len(all_hand_landmarks) < 200:
+            remaining = 200 - len(all_hand_landmarks)
+            all_hand_landmarks.extend([[0.0] * 84] * remaining)
+
+    cap.release()
+    return all_hand_landmarks
+
+
+
+
 @app.route('/scrape', methods=['GET'])
 def scrape():
     word = request.args.get('word', default='', type=str)
@@ -60,6 +134,33 @@ def scrape():
         return jsonify({'error': video_url}), 404
 
     return jsonify({'mp4_url': video_url})
+
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+    file = request.files['file']
+    print(file)
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    if file:
+        file_path = file.filename
+        #file.save(file_path)
+        hand_pose_data = extract_frames(file_path)
+        prediction = callModel(hand_pose_data)
+
+        print(prediction)
+
+
+        
+
+
+
+
+        return jsonify({'message': 'File successfully uploaded and frames extracted', 'file_path': file_path, 'predicted_word': prediction}), 200
+
 
 
 if __name__ == '__main__':
